a2fee4926c67f9d0203b27b9c865458eef8b3223
micha vie <micha@freeiam.com>
2022-12-20 19:22:48 +0100

    fix itheum app

--- extensions/itheum/Itheum.tsx
+++ extensions/itheum/Itheum.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { AppRootProps } from '../../src/types'
 import { useApp } from '../../src/hooks/useApp'
 import { AppSection } from '../../src/ui/extension/AppSection'
@@ -5,9 +6,7 @@ import { AppSection } from '../../src/ui/extension/AppSection'
 export const Itheum = (props: AppRootProps) => {
   const app = useApp(props)
 
-  const handleSubmit = () => {
-    // app.requestProposalAction()
-  }
+  console.log(app)
 
   return (
     <div>
