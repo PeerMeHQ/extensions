6f3fd0e74ec025fca8dab0d6e240150c95f0b45b
micha vie <micha@freeiam.com>
2023-02-10 21:52:08 +0100

    add app selector search

--- src/ui/AppSelector.tsx
+++ src/ui/AppSelector.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Config } from '../config'
+import { Input } from '@peerme/web-ui'
 import { useEffect, useState } from 'react'
 import { _AppPresenter } from './_AppPresenter'
-import { ProposalAction } from '@peerme/core-ts'
 import { _AppSelectorItem } from './_AppSelectorItem'
 import { ExtensionInfo, ExtensionConfig } from '../types'
+import { ProposalAction, useDebounce } from '@peerme/core-ts'
 
 type NotificationType = 'success' | 'info' | 'warning' | 'error' | 'vibe'
 
@@ -17,21 +18,29 @@ type Props = {
 
 export const AppSelector = (props: Props) => {
   const [activeApp, setActiveApp] = useState<ExtensionInfo | null>(null)
-
-  const availableApps = Config.Extensions.filter((app) => app.Enabled || props.config.hasEarlyAccess)
+  const [availableApps, setAvailableApps] = useState(getAvailableApps(props.config))
+  const [searchQuery, setSearchQuery] = useState('')
+  const debouncedSearchQuery = useDebounce(searchQuery, 200)
 
   useEffect(() => {
     if (props.onAppSelected) props.onAppSelected(activeApp)
   }, [activeApp])
+
+  useEffect(() => {
+    setAvailableApps(getSearchResults(props.config, debouncedSearchQuery))
+  }, [debouncedSearchQuery])
 
   return (
     <section>
       {activeApp === null && (
         <header className="mb-4">
           <h2 className="mb-1">Third-party Apps</h2>
-          <p className="text-xl">
+          <p className="text-xl mb-2">
             Peering DAOs can interact easily interact with the third-party apps you love the most.
           </p>
+          {props.config.hasEarlyAccess && (
+            <Input placeholder="Search Apps ..." value={searchQuery} onChange={(val) => setSearchQuery(val)} />
+          )}
         </header>
       )}
       {activeApp ? (
@@ -48,3 +57,11 @@ export const AppSelector = (props: Props) => {
     </section>
   )
 }
+
+const getAvailableApps = (config: ExtensionConfig) =>
+  Config.Extensions.filter((app) => app.Enabled || config.hasEarlyAccess)
+
+const getSearchResults = (config: ExtensionConfig, query: string) =>
+  query.length === 0
+    ? getAvailableApps(config)
+    : getAvailableApps(config).filter((app) => app.Name.toLowerCase().includes(query.toLowerCase()))
