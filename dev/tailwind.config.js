6d86a1636cacb253c57f8e2b10e58bdf66457a77
micha vie <micha@freeiam.com>
2023-02-17 23:54:59 +0100

    fix tailwind config

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
+    '../src/**/*.{js,ts,jsx,tsx}',
     './node_modules/@peerme/extensions/**/*.{js,ts,jsx,tsx}',
     './node_modules/@peerme/web-ui/**/*.{js,ts,jsx,tsx}',
   ],
