1a2ef7d35fc57907eef2aa3ad6568ae2cb7a6af8
micha vie <micha@freeiam.com>
2023-02-18 15:14:55 +0100

    update coindrip stream creator

--- src/extensions/coindrip/src/_StreamCreator.tsx
+++ src/extensions/coindrip/src/_StreamCreator.tsx
@@ -2,7 +2,7 @@ import { getContractAddress } from './config'
 import { AppHook } from '../../../hooks/useApp'
 import { TokenPayment } from '@elrondnetwork/erdjs'
 import React, { SyntheticEvent, useState } from 'react'
-import { Input, Button, UserSelector, PaymentSelector } from '@peerme/web-ui'
+import { Input, Button, UserSelector, PaymentSelector, Switch } from '@peerme/web-ui'
 
 type Props = {
   app: AppHook
@@ -15,23 +15,33 @@ export const _StreamCreator = (props: Props) => {
   const [payment, setPayment] = useState<TokenPayment | null>(null)
   const [startsAt, setStartsAt] = useState('')
   const [endsAt, setEndsAt] = useState('')
+  const [cancellable, setCancellable] = useState(true)
+  const isSubmitDisabled = !recipient || !payment || !startsAt || !endsAt
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
-    if (!payment) {
-      props.app.showToast('Please add a payment', 'error')
-      return
-    }
+    if (!payment) return
+    const nowTs = Math.floor(Date.now() / 1000)
     const startsAtTs = Math.floor(new Date(startsAt).getTime() / 1000)
     const endsAtTs = Math.floor(new Date(endsAt).getTime() / 1000)
     const value = payment.isEgld() ? payment.amountAsBigInteger : 0
     const tokenPayments = payment.isEgld() ? [] : [payment]
 
+    if (startsAtTs < nowTs) {
+      props.app.showToast('Start date must be set to the future', 'error')
+      return
+    }
+
+    if (endsAtTs < startsAtTs) {
+      props.app.showToast('Start date must be before end date', 'error')
+      return
+    }
+
     props.app.requestProposalAction(
       getContractAddress(props.app.config.network),
       ContractEndpoint,
       value,
-      [recipient, startsAtTs, endsAtTs],
+      [recipient, startsAtTs, endsAtTs, cancellable],
       tokenPayments
     )
   }
@@ -54,31 +64,40 @@ export const _StreamCreator = (props: Props) => {
         onSelected={(val) => setPayment(val)}
         className="mb-8"
       />
-      <label htmlFor="starting_date" className="pl-1 text-xl mb-2 text-gray-800 dark:text-gray-200">
-        Starts at (your local time)
-      </label>
-      <Input
-        id="starting_date"
-        type="datetime-local"
-        placeholder="Starts at ..."
-        value={startsAt}
-        onChange={(val) => setStartsAt(val)}
-        className="mb-4"
-        required
-      />
-      <label htmlFor="ending_date" className="pl-1 text-xl mb-2 text-gray-800 dark:text-gray-200">
-        Ends at (your local time)
-      </label>
-      <Input
-        id="ending_date"
-        type="datetime-local"
-        placeholder="Ends at ..."
-        value={endsAt}
-        onChange={(val) => setEndsAt(val)}
-        className="mb-8"
-        required
-      />
-      <Button color="blue" className="block w-full" submit>
+      <div className="mb-4">
+        <label htmlFor="starting_date" className="pl-1 text-xl mb-2 text-gray-800 dark:text-gray-200">
+          Starts at (your local time)
+        </label>
+        <Input
+          id="starting_date"
+          type="datetime-local"
+          placeholder="Starts at ..."
+          value={startsAt}
+          onChange={(val) => setStartsAt(val)}
+          required
+        />
+        <small className="block mt-2 pl-2 text-yellow-500 text-sm">
+          The proposal must be executed before the start date or it will fail.
+        </small>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="ending_date" className="pl-1 text-xl mb-2 text-gray-800 dark:text-gray-200">
+          Ends at (your local time)
+        </label>
+        <Input
+          id="ending_date"
+          type="datetime-local"
+          placeholder="Ends at ..."
+          value={endsAt}
+          onChange={(val) => setEndsAt(val)}
+          required
+        />
+      </div>
+      <div className="flex items-center space-x-4 py-4 mb-4">
+        <Switch label="Cancellable" checked={cancellable} onChange={(val) => setCancellable(val)} />
+        <span className="text-xl text-gray-700 dark:text-gray-200">Can cancel?</span>
+      </div>
+      <Button color="blue" className="block w-full" disabled={isSubmitDisabled} submit>
         Create Stream
       </Button>
     </form>
