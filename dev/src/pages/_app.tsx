efc016b042d4fc9691f59d096aa89bd5156e51ff
micha vie <micha@freeiam.com>
2023-02-18 15:14:22 +0100

    add dev-server toast support

--- dev/src/pages/_app.tsx
+++ dev/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from '@peerme/web-ui'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  )
 }
