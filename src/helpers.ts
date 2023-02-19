411df74e525e8c440683abb4dbeab6d82e2e3666
micha vie <micha@freeiam.com>
2023-02-19 17:56:53 +0100

    add extension find by contract utility

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,22 @@
+import { Config } from './config'
 import BigNumber from 'bignumber.js'
 import { TokenPayment } from '@elrondnetwork/erdjs'
 import { ExtensionConfig, ExtensionInfo } from './types'
 import { ProposalAction, ProposalActionArg, toProposalActionPayment } from '@peerme/core-ts'
+
+export const findExtensionByContract = (
+  config: ExtensionConfig,
+  address: string,
+  endpoint?: string
+): ExtensionInfo | null =>
+  Config(config).Extensions.find((extension) =>
+    Object.values(extension.Contracts).some((contract) => {
+      if (contract.Address === address) {
+        return endpoint ? contract.Endpoint === endpoint : true
+      }
+      return false
+    })
+  ) || null
 
 export const toExtensionName = (config: ExtensionConfig, extension: ExtensionInfo) =>
   extension.Name.replace(':entityName', config.entity.name)
