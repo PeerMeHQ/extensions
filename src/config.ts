f5fa1f66f380df2e864380ef67a18d672de885e4
micha vie <micha@freeiam.com>
2023-02-19 19:36:39 +0100

    fix circular dep

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { ExtensionConfig } from './types'
+import { ExtensionConfig, ExtensionInfo } from './types'
 import { DaoExtension } from './extensions/dao/register'
 import { XBulkExtension } from './extensions/xbulk/register'
 import { ItheumExtension } from './extensions/itheum/register'
@@ -19,3 +19,17 @@ export const Config = (config: ExtensionConfig) => ({
     Extensions: 'https://know.peerme.io/developers/extensions.html',
   },
 })
+
+export const findExtensionByContract = (
+  config: ExtensionConfig,
+  address: string,
+  endpoint?: string
+): ExtensionInfo | null =>
+  Config(config).Extensions.find((extension) =>
+    Object.values(extension.Contracts).some((contract) => {
+      if (contract.Address === address) {
+        return endpoint ? contract.Endpoint === endpoint : true
+      }
+      return false
+    })
+  ) || null
