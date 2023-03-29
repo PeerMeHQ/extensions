59b9983edbc789dc525995122f7eb4ad9d4fbeb7
micha vie <micha@freeiam.com>
2023-03-29 17:41:06 +0200

    further itheum procuring updates

--- src/extensions/itheum/src/market/_OfferDetails.tsx
+++ src/extensions/itheum/src/market/_OfferDetails.tsx
@@ -1,18 +1,11 @@
 import Link from 'next/link'
-import { Config } from '../config'
-import { Button } from '@peerme/web-ui'
-import { BigNumber } from 'bignumber.js'
-import { Contracts } from '../contracts'
-import { Constants } from '@peerme/core-ts'
+import { _Procurer } from './_Procurer'
 import { decodeNftMetadata } from '../helpers'
 import React, { useEffect, useState } from 'react'
-import { TokenPayment } from '@multiversx/sdk-core'
 import { DataNftMetadata, OfferInfo } from '../types'
 import { useApp } from '../../../../shared/hooks/useApp'
-import { AppContextValue } from '../../../../shared/types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLink } from '@fortawesome/free-solid-svg-icons'
-import { NonFungibleTokenOfAccountOnNetwork } from '@multiversx/sdk-network-providers'
 
 type Props = {
   offer: OfferInfo
@@ -20,48 +13,28 @@ type Props = {
 
 export const _OfferDetails = (props: Props) => {
   const app = useApp()
-  const [nft, setNft] = useState<NonFungibleTokenOfAccountOnNetwork | null>(null)
-  const [nftMetadata, setNftMetadata] = useState<DataNftMetadata | null>(null)
-  const [quantity] = useState(1)
+  const [nft, setNft] = useState<DataNftMetadata | null>(null)
 
   useEffect(() => {
     app.networkProvider
       .getNonFungibleToken(props.offer.offeredTokenIdentifier, props.offer.offeredTokenNonce)
-      .then((val) => {
-        setNft(val)
-        setNftMetadata(decodeNftMetadata(val))
-      })
+      .then((val) => setNft(decodeNftMetadata(val)))
   }, [])
 
-  const handleProcure = () => {
-    if (+quantity > +props.offer.quantity || +quantity <= 0) {
-      app.showToast('Invalid quantity', 'error')
-      return
-    }
-    const contract = Contracts(app.config).AcceptOffer
-    const wantedAmount = props.offer.wantedTokenAmount.multipliedBy(quantity)
-    const wantsEgld = props.offer.wantedTokenIdentifier == Constants.EgldTokenIdentifier
-    const value = wantsEgld ? wantedAmount : 0
-    const tokenPayments =
-      wantsEgld || wantedAmount.isZero() ? [] : [toWantedTokenPayment(app, props.offer, wantedAmount)]
-
-    app.requestProposalAction(contract.Address, contract.Endpoint, value, [props.offer.index, +quantity], tokenPayments)
-  }
-
-  if (!nft || !nftMetadata) return null
+  if (!nft) return null
 
   return (
     <div className="flex flex-col sm:flex-row sm:gap-8 py-4">
-      <img src={nftMetadata.nftImgUrl} alt="NFT Preview" className="w-40 h-40 sm:w-64 sm:h-64" />
+      <img src={nft.nftImgUrl} alt="NFT Preview" className="w-40 h-40 sm:w-64 sm:h-64" />
       <div className="flex flex-col gap-4">
-        <h2 className="text-2xl">{nftMetadata?.title}</h2>
+        <h2 className="text-2xl">{nft?.title}</h2>
         <Link
-          href={app.config.walletConfig.Explorer + '/nfts/' + nft.identifier}
+          href={app.config.walletConfig.Explorer + '/nfts/' + nft.id}
           target="_blank"
           rel="noreferrer"
           className="text-blue-500 hover:text-blue-400"
         >
-          {nft.identifier + ' '}
+          {nft.id + ' '}
           <FontAwesomeIcon icon={faExternalLink} />
         </Link>
         <ul className="list-none text-gray-700 dark:text-gray-300 text-lg">
@@ -72,19 +45,8 @@ export const _OfferDetails = (props: Props) => {
             Total supply: <strong>{nft.supply.toFixed()}</strong>
           </li>
         </ul>
-        <Button onClick={handleProcure} color="blue">
-          Procure
-        </Button>
+        <_Procurer offer={props.offer} nft={nft} />
       </div>
     </div>
   )
-}
-
-const toWantedTokenPayment = (app: AppContextValue, offer: OfferInfo, amount: BigNumber.Value) =>
-  new TokenPayment(offer.wantedTokenIdentifier, offer.wantedTokenNonce, amount, getWantedTokenDecimals(app, offer))
-
-const getWantedTokenDecimals = (app: AppContextValue, offer: OfferInfo) => {
-  if (offer.wantedTokenIdentifier == Constants.EgldTokenIdentifier) return Constants.EgldDecimals
-  if (offer.wantedTokenIdentifier == Config.TokenId(app.config.network)) return Config.TokenDecimals
-  return 0
 }
