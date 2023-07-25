884fa2e160b5157e422252b52f933e8e080afb71
micha vie <micha@freeiam.com>
2023-07-25 18:14:54 +0200

    update build system

--- src/shared/ui/_AppLogo.tsx
+++ src/shared/ui/_AppLogo.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import { ExtensionInfo, ExtensionConfig } from '../types'
 
 type Props = {
@@ -12,28 +11,14 @@ type Props = {
 export const _AppLogo = (props: Props) => {
   if (props.config.dark) {
     return typeof props.extension.Logo.Dark === 'string' ? (
-      <Image
-        src={props.extension.Logo.Dark}
-        alt={props.extension.Name + ' Logo'}
-        className={props.className}
-        width={props.size}
-        height={props.size}
-        unoptimized
-      />
+      <img src={props.extension.Logo.Dark} alt={props.extension.Name + ' Logo'} className={props.className} />
     ) : (
       <props.extension.Logo.Dark className={props.className} width={props.size} height={props.size} />
     )
   }
 
   return typeof props.extension.Logo.Light === 'string' ? (
-    <Image
-      src={props.extension.Logo.Light}
-      alt={props.extension.Name + ' Logo'}
-      className={props.className}
-      width={props.size}
-      height={props.size}
-      unoptimized
-    />
+    <img src={props.extension.Logo.Light} alt={props.extension.Name + ' Logo'} className={props.className} />
   ) : (
     <props.extension.Logo.Light className={props.className} width={props.size} height={props.size} />
   )
