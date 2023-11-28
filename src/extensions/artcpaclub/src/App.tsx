41e926f2bc69922957dc6fb0712e6a61df6e7fb4
Micha Vie <micha@freeiam.com>
2023-11-28 18:09:04 +0100

    add artcpaclub app tabs

--- src/extensions/artcpaclub/src/App.tsx
+++ src/extensions/artcpaclub/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { useApp } from '../../../shared/hooks/useApp'
-import { AppSection } from '../../../shared/ui/elements'
+import { Tab } from '@headlessui/react'
+import { EsdtTab } from './esdt/EsdtTab'
+import { TabButton } from '../../../shared/ui/elements'
+import { faCoins, faSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const App = () => {
-  const app = useApp()
-
-  // The app hook provides access to the extension configuration
-  // and useful methods for interacting with DAOs.
-  console.log(app)
-
   return (
-    <div>
-      <AppSection title="Example Section">
-        <p>Example paragraph</p>
-      </AppSection>
-    </div>
+    <Tab.Group>
+      <Tab.List className="flex items-center space-x-2 md:space-x-4 mb-4">
+        <TabButton icon={faCoins}>ESDT Staking</TabButton>
+        <TabButton icon={faSquare}>NFT Staking</TabButton>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>
+          <EsdtTab />
+        </Tab.Panel>
+        <Tab.Panel>
+          <p>Coming soon.</p>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
   )
 }
---
+++ src/extensions/artcpaclub/src/esdt/_Staker.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useApp } from '../../../../shared/hooks/useApp'
+import { AppSection } from '../../../../shared/ui/elements'
+
+export function _Staker() {
+  const app = useApp()
+
+  return (
+    <AppSection title="Stake now">
+      <p>Coming soon.</p>
+    </AppSection>
+  )
+}
