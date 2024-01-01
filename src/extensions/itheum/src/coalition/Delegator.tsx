0c0931ce801f57857a492763d3fd64c1a3914fd0
Micha Vie <micha@freeiam.com>
2024-01-02 00:50:28 +0100

    update itheum coalition delegator

--- src/extensions/itheum/src/coalition/Delegator.tsx
+++ src/extensions/itheum/src/coalition/Delegator.tsx
@@ -5,7 +5,7 @@ import { ScInfo, useScQuery } from '@peerme/core-ts'
 import React, { useEffect, useState } from 'react'
 import { toTypedAggregatorAppInfo } from '../helpers'
 import { useApp } from '../../../../shared/hooks/useApp'
-import { Select, SelectOption, Theme } from '@peerme/web-ui'
+import { Button, Select, SelectOption, Theme } from '@peerme/web-ui'
 import { Contracts, getCoalitionContractAddress } from '../contracts'
 import { AggregatorAppInfo, CoalitionInfo, DataNftMetadata } from '../types'
 import {
@@ -26,7 +26,8 @@ type Props = {
 export function Delegator(props: Props) {
   const app = useApp()
   const [appInfo, setAppInfo] = useState<AggregatorAppInfo | null>(null)
-  const [collections, setCollections] = useState<Record<string, DataNftMetadata[]>>({})
+  const [selected, setSelected] = useState<Record<string, DataNftMetadata[]>>({})
+  const [userCollections, setUserCollections] = useState<Record<string, DataNftMetadata[]>>({})
   const [category, setCategory] = useState<string | null>(null)
 
   const appInfoScInfo: ScInfo = { ...Contracts(app.config).GetAppInfo, Address: props.info.aggregator }
@@ -52,12 +53,29 @@ export function Delegator(props: Props) {
   useEffect(() => {
     if (!appInfo) return
     fetchDataNftsOfAccount(app, appInfo?.dataCollections).then((data) => {
-      setCollections(collect(data).groupBy('collection').all() as any)
+      setUserCollections(collect(data).groupBy('collection').all() as any)
     })
   }, [appInfo])
 
-  const handleDelegate = (delegatables: TokenTransfer[]) => {
-    if (!app.config.user || !category) return
+  const toggleSelected = (nft: DataNftMetadata) => {
+    if (!selected[nft.collection]) {
+      setSelected({ ...selected, [nft.collection]: [nft] })
+    } else if (isSelected(nft)) {
+      setSelected({ ...selected, [nft.collection]: selected[nft.collection].filter((d) => d.nonce !== nft.nonce) })
+    } else {
+      setSelected({ ...selected, [nft.collection]: [...selected[nft.collection], nft] })
+    }
+  }
+
+  const isSelected = (nft: DataNftMetadata) => !!selected[nft.collection]?.find((d) => d.nonce === nft.nonce)
+
+  const handleDelegate = () => {
+    console.log('delegate', app.config.user, category, selected)
+    if (!app.config.user || !category || !Object.keys(selected).length) return
+    console.log('aaa')
+    const amount = 1 // default for NFTs, and for SFTs only 1 is allowed since data stream is equal
+    const nfts = Object.values(selected).flat(1)
+    const transferables = nfts.map((nft) => TokenTransfer.semiFungible(nft.collection, nft.nonce, amount))
     const contract = new SmartContract({ address: Address.fromBech32(getCoalitionContractAddress(app.config.network)) })
     const tx = new Interaction(contract, new ContractFunction('grantAccess'), [
       new AddressValue(Address.fromBech32(app.config.entity.address)),
@@ -65,18 +83,18 @@ export function Delegator(props: Props) {
     ])
       .withChainID(app.config.walletConfig.ChainId)
       .withSender(new Address(app.config.user.address))
-      .withGasLimit(10_000_000)
-      .withMultiESDTNFTTransfer(delegatables)
+      .withGasLimit(50_000_000)
+      .withMultiESDTNFTTransfer(transferables)
       .buildTransaction()
     app.requestUserAction(tx)
   }
 
   return (
-    <div className={props.className} style={{ maxHeight: '80vh' }}>
+    <div className={props.className}>
       <h2 className="mb-2">Delegate Data to {app.config.entity.name}</h2>
       <p className="mb-4">Grant access to the Data Coalition to use your data. You can revoke access at any time.</p>
-      <ul className="space-y-4">
-        {Object.entries(collections).map(([collection, nfts]) => (
+      <ul className="space-y-4 overflow-y-auto" style={{ maxHeight: '80vh' }}>
+        {Object.entries(userCollections).map(([collection, nfts]) => (
           <li key={collection} className={clsx('px-6 py-4', Theme.Background.Subtle, Theme.BorderRadius.Subtle)}>
             <h3 className="mb-2">
               <span>{collection.split('-')[0]}</span>
@@ -85,42 +103,52 @@ export function Delegator(props: Props) {
             <ul className="flex flex-wrap gap-2">
               {nfts.map((nft) => (
                 <li key={nft.nonce}>
-                  <button onClick={() => handleDelegate([TokenTransfer.semiFungible(nft.collection, nft.nonce, 1)])}>
+                  <button type="button" onClick={() => toggleSelected(nft)}>
                     <img
                       src={nft.nftImgUrl}
                       alt={nft.title}
                       className={clsx(
                         'duration-400 h-16 w-16 transform rounded-lg transition active:translate-y-1 cursor-pointer hover:shadow-lg',
-                        false /* delegated */ ? '' : 'opacity-50 grayscale',
-                        false /* selected */ ? 'shadow-xl' : 'opacity-25 shadow-inner'
+                        true /* delegated */ ? '' : 'opacity-50 grayscale',
+                        isSelected(nft) ? 'shadow-xl' : 'opacity-25 shadow-inner'
                       )}
                     />
                   </button>
                 </li>
               ))}
             </ul>
-            <div>
-              <label
-                htmlFor="category"
-                className="mb-2 pl-1 text-base sm:text-lg text-gray-800 dark:text-gray-200 md:text-xl"
-              >
-                Category
-              </label>
-              <Select
-                id="category"
-                options={toCategoryOptions(props.info.categories)}
-                onSelect={(val) => setCategory(val)}
-              />
-            </div>
           </li>
         ))}
       </ul>
+      {Object.keys(selected).length > 0 && (
+        <div className="mt-4">
+          <div className="mb-4">
+            <label
+              htmlFor="category"
+              className="mb-2 pl-1 text-base sm:text-lg text-gray-800 dark:text-gray-200 md:text-xl"
+            >
+              Category
+            </label>
+            <Select
+              id="category"
+              options={toCategoryOptions(props.info.categories)}
+              onSelect={(val) => setCategory(val)}
+            />
+          </div>
+          <Button color="blue" onClick={handleDelegate} className="block w-full" disabled={!category}>
+            Delegate
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
 
-const toCategoryOptions = (categories: string[]): SelectOption[] =>
-  categories.map((category) => ({
+const toCategoryOptions = (categories: string[]): SelectOption[] => {
+  const options = categories.map((category) => ({
     value: category,
     name: category,
   }))
+
+  return [{ value: '-', name: 'Select Category' }, ...options]
+}
