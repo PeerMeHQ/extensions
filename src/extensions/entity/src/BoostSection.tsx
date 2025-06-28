5ee972483f2f31e417fcf3115e707ba6a9b4e1c1
Micha Vie <micha@vleap.ai>
2025-06-28 15:36:44 +0200

    add boost multi-stable support

--- src/extensions/entity/src/BoostSection.tsx
+++ src/extensions/entity/src/BoostSection.tsx
@@ -1,11 +1,9 @@
 import { TokenTransfer } from '@multiversx/sdk-core/out'
-import { Constants } from '@peerme/core-ts'
-import { Alert, Button, EntityTransferSelector } from '@peerme/web-ui'
+import { Button, EntityTransferSelector } from '@peerme/web-ui'
 import { address } from '@vleap/warps'
 import React, { SyntheticEvent, useState } from 'react'
 import { useApp } from '../../../shared/hooks/useApp'
 import { AppSection } from '../../../shared/ui/elements/AppSection'
-import { EntityConfig } from './config'
 import { EntityContracts } from './contracts'
 
 type Props = {
@@ -16,12 +14,9 @@ export function BoostSection(props: Props) {
   const app = useApp()
   const [payment, setPayment] = useState<TokenTransfer | null>(null)
 
-  const isInvalidPayment =
-    !!payment && !payment.isEgld() && payment.tokenIdentifier !== EntityConfig.StableTokenId(app.config.network.env)
-
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
-    if (!payment || payment.amountAsBigInteger.isZero()) {
+    if (!payment || payment.amount === 0n) {
       app.showToast('Can not boost with zero', 'error')
       return
     }
@@ -52,11 +47,6 @@ export function BoostSection(props: Props) {
           onSelected={(val) => setPayment(val)}
           className="mb-8"
         />
-        {isInvalidPayment && (
-          <Alert type="warning" className="mb-8">
-            Please select {Constants.Egld.Id} or {EntityConfig.StableTokenName} to boost.
-          </Alert>
-        )}
         <Button color="blue" className="block w-full" disabled={!payment} type="submit">
           Boost now
         </Button>
